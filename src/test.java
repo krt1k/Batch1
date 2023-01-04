ad9f0c4ca768f52b05f27cd315dce82cf9ea98d7
krth1k <Kishorekarthik98@gmail.com>
2023-01-04 10:44:06 +0530

    test.java changed

--- src/test.java
+++ src/test.java
@@ -1,64 +1,24 @@
 import java.util.Scanner;
 
 public class test{
-    private Node head;
-    private Node tail;
-    private int size;
-
-    class Node{
-        int data;
-        Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public test(){
-        size = 0;
-    }
-
-    public void add(int data){
-        Node n = new Node(data);
-
-        if(head == null)
-            head = n;
-        else{
-            Node newnode = head;
-
-            while(newnode.next != null){
-                newnode = newnode.next;
-            }
-
-            newnode.next = n;
-        }
-
-    }
-
-    public void printAtest(){
-        Node n = head;
-
-        while(n.next!=null){
-            System.out.print(n.data+" ");
-            n = n.next;
-        }
-        System.out.print(n.data);
-
-    }
-
     public static void main(String[] args){
+
         Scanner pk = new Scanner(System.in);
-        test e = new test();
 
-        int a = pk.nextInt();
+        int a1 = pk.nextInt();
 
-        for(int i=0; i<a; i++)
-            e.add(pk.nextInt());
+        String b = pk.nextLine();
 
-        e.printAtest();
+        for (int i = 0; i < b.length(); i++) {
+            System.out.print(b.charAt(i)+" ");
+        }
+
+
+        String a = "kishoreKarthik";
+
+
+        a = a.toLowerCase();
+
+        System.out.println(a);
     }
-
-
-
 }
